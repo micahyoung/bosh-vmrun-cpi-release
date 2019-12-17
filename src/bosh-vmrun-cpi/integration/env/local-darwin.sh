5e5a21aa01a709a6d5017f5a1b23eb79df05d5fc
Micah Young <myoung@pivotal.io>
2019-12-16 22:02:19 -0500

    Add env script for darwin

---
+++ src/bosh-vmrun-cpi/integration/env/local-darwin.sh
@@ -0,0 +1,6 @@
+export PATH=$PATH:/Applications/VMware\ Fusion.app/Contents/Library:/Applications/VMware\ Fusion.app/Contents/Library/VMware\ OVF\ Tool/
+export SSH_USERNAME=$USER
+export SSH_HOSTNAME="localhost"
+export SSH_PORT="22"
+export SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa_vmrun)"
+export SSH_PLATFORM="darwin"
